b3d8f33002a2d0e67cedb1cc45c98722ca8aaa5a
KSydorenko <koskossid@gmail.com>
2020-10-23 15:14:52 +0300

    develop [material UI initial settings]

---
+++ client/src/pages/NotFound/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import { withRouter } from 'react-router'
+import Container from '@material-ui/core/Container'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  errorContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    alignItems: 'center',
+  },
+  btn: {
+    margin: 20,
+  },
+}))
+function NotFound(props) {
+  const classes = useStyles()
+  const { history } = props
+
+  return (
+    <Container className={classes.root}>
+      <Grid container spacing={2} className={classes.errorContainer}>
+        <Typography variant="h1">404</Typography>
+        <Typography variant="h6">Page is not exist...</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.btn}
+          onClick={history.goBack}
+        >
+          Go Back
+        </Button>
+      </Grid>
+    </Container>
+  )
+}
+
+export default withRouter(NotFound)
---
+++ client/src/theme/Theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#112667',
+      light: '#474e96',
+      dark: '#00003c',
+      contrastText: '#ffffff ',
+    },
+    secondary: {
+      main: '#0369b4',
+      light: '#5696e7',
+      dark: '#003f84',
+      contrastText: '#ffffff ',
+    },
+    grays: {
+      gray: '#9E9E9E',
+      blueGary: '#607D8B',
+      black: '#040404',
+    },
+  },
+})
