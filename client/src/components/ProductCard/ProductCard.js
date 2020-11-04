fa1836afefdd4d7d357f9910eb828b848c7ba51a
vchmichael <vchmichael@gmail.com>
2020-11-04 16:02:29 +0200

    cards

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,17 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import IconButton from '@material-ui/core/IconButton'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import StarIcon from '@material-ui/icons/Star'
-import StarBorderIcon from '@material-ui/icons/StarBorder'
 import FavoriteIcon from '@material-ui/icons/Favorite'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: '80%',
+    maxWidth: '100%',
     display: 'flex',
     padding: 10,
     margin: 15,
@@ -19,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-between',
   },
   media: {
-    width: '200px',
+    width: '150px',
   },
   status: {
     border: '2px solid #112667',
@@ -28,7 +27,7 @@ const useStyles = makeStyles((theme) => ({
     padding: '4px 20px',
   },
   colorBlock: {
-    width: 50,
+    // width: 30,
     display: 'flex',
     flexDirection: 'column',
   },
@@ -39,63 +38,72 @@ const useStyles = makeStyles((theme) => ({
     border: '1px solid #fff',
     borderRadius: '10px',
   },
-  // className: {
-  //   display: 'flex',
-  //   color: 'red'
-  // }
+  actionsBlock: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  rightCardBlock: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
 }))
-
-// <div className={classes.className}> </div>
 
 export const ProductCard = (props) => {
   const classes = useStyles()
-  const { guitar } = props
+  const { element } = props
+  const [isFavorite, setFavorite] = React.useState(false)
 
-  const [rated, setRated] = useState(false)
-
-  const handleClickRate = () => {
-    setRated(!rated)
+  const handleFavorite = () => {
+    setFavorite(!isFavorite)
   }
 
   return (
     <Card className={classes.root}>
       <div className={classes.colorBlock}>
-        {guitar.availableColors.map((e) => (
-          <div className={classes.colorItem} style={{ background: e }} />
-        ))}
+        <div
+          className={classes.colorItem}
+          style={{ background: element.color }}
+        />
       </div>
-      <img className={classes.media} src={guitar.img} alt={guitar.title} />
+      <img className={classes.media} src={'/guitar.png'} alt="guitar" />
       <CardContent>
         <div style={{ display: 'flex' }}>
-          <Typography variant="h4">{guitar.title}</Typography>
-          <Typography variant="body1" className={classes.status}>
-            {guitar.status}
+          <Typography variant="h6" style={{ textTransform: 'uppercase' }}>
+            {element.name}
           </Typography>
         </div>
-        {guitar.rating.map((e) => {
-          return rated ? (
-            <StarBorderIcon onClick={handleClickRate} />
-          ) : (
-            <StarIcon onClick={handleClickRate} />
-          )
-        })}
-
-        <Typography variant="body2"></Typography>
-        {guitar.price}
-        <Typography variant="body2">Тип конструкции: {guitar.type}</Typography>
-        <Typography variant="body2">Корпус: {guitar.body}</Typography>
-        <Typography variant="body2">Гриф: {guitar.griff}</Typography>
-        <Typography variant="body2">
-          Накладка грифа: {guitar.fretboard}
-        </Typography>
+        <Typography variant="body2">Цена: {element.currentPrice}</Typography>
+        <Typography variant="body2">Код товара: {element.itemNo}</Typography>
+        <Typography variant="body2">Бренд: {element.brand}</Typography>
+        <Typography variant="body2">Гриф: {element.griff}</Typography>
+        <Typography variant="body2">Корпус: {element.guitarBody}</Typography>
       </CardContent>
-      <div style={{ width: '15%' }}>
-        <Button variant="contained" color="primary">
-          Add to cart
-        </Button>
-        <IconButton aria-label="like">
-          <FavoriteIcon />
-        </IconButton>
+      <div className={classes.rightCardBlock}>
+        <div className={classes.actionsBlock}>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            className={classes.button}
+            startIcon={<ShoppingCartIcon style={{ color: '#fff' }} />}
+          >
+            В корзину
+          </Button>
+          <IconButton aria-label="like" style={{ width: 50 }}>
+            {isFavorite ? (
+              <FavoriteIcon
+                style={{ color: '#C22A2A' }}
+                onClick={handleFavorite}
+              />
+            ) : (
+              <FavoriteIcon onClick={handleFavorite} />
+            )}
+          </IconButton>
+        </div>
+        <Typography variant="body1" className={classes.status}>
+          {element.enabled ? `В наличии: ${element.quantity}` : 'Отсутствует'}
+        </Typography>
       </div>
     </Card>
   )
