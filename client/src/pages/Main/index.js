a6193ac719c7a85f097ec73a85c61ad4db984bb5
Nataly <67338123+Lilion-Loly@users.noreply.github.com>
2020-11-18 21:10:40 +0200

    bestsellers [28YjW4vB] : [create_bestsellers-slider] (#47)

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -4,6 +4,7 @@ import Container from '@material-ui/core/Container'
 import { getSlides } from '../../store/slides/slidesActions'
 import ProductSlides from '../../components/Slides/ProductSlides'
 import Categories from '../../components/Categories'
+import BestsellersSlider from '../../components/BestsellersSlider'
 
 function Main() {
   const dispatch = useDispatch()
@@ -13,6 +14,7 @@ function Main() {
   return (
     <Container>
       <ProductSlides />
+      <BestsellersSlider />
       <Categories />
     </Container>
   )
